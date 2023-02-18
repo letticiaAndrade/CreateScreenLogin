9a561ccb3ec783b4c7bfc1821bdf1092a4052d46
Leticia <leticiaae@hotmail.com.br>
2023-02-18 19:25:31 -0300

    adicionando botão back no header

--- src/screen/Login.jsx
+++ src/screen/Login.jsx
@@ -4,11 +4,10 @@ import { Ionicons } from '@expo/vector-icons';
 
 export const Login= ({navigation})=> {
     return (
-        <View style={{flex:1}}>
-            <View>
-                <Ionicons.Button name="arrow-back" size={30} color={'black'} backgroundColor='white' onPress={()=> navigation.navigate('Home')}
-                 style={{margin: 20}}>
-
+        <View style={{flex:1,backgroundColor:'white'}}>
+            <View style={{ padding:33, height:100,width:120 }}>
+                <Ionicons.Button name="arrow-back" size={30} color={'black'} backgroundColor='white' onPress={()=> navigation.navigate('Home')} activeOpacity={1}
+               iconStyle={{width:30, height:30}}>
                 </Ionicons.Button>
             </View>
 
