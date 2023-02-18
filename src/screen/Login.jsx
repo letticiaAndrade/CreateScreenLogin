cb0a84f857fbcdb8756787d613c8b14097f27490
Leticia <leticiaae@hotmail.com.br>
2023-02-18 18:42:51 -0300

    alterando tela de Login

--- src/screen/Login.jsx
+++ src/screen/Login.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
 
-export const Login= ()=> {
+export const Login= ({navigation})=> {
     return (
-        <View style={{backgroundColor: 'blue', flex:1}}>
-            <Text>OI</Text>
+        <View style={{flex:1}}>
+            <View>
+                <Ionicons.Button name="arrow-back" size={30} color={'black'} backgroundColor='white' onPress={()=> navigation.navigate('Home')}
+                 style={{margin: 20}}>
+
+                </Ionicons.Button>
+            </View>
+
+            <View style={{backgroundColor:"red"}}>
+                <Text style={{fontWeight:'bold', fontSize:30}}>Welcome!</Text>
+            </View>
+           
         </View>
     )
 }
